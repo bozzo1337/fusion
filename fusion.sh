1783276a6e03510a9c40af1dd9fe72d80ce047aa
Benjamin <benjamin.longa@gmail.com>
2020-04-06 14:14:08 +0200

    Utilisation base64

--- fusion.sh
+++ fusion.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch -n 10 'thermalCam/thermalCam ; mosquitto_pub -h 192.168.0.22 -p 1883 -t '"'"'fusion/ir'"'"' -f '"'"'thermalCam/testIR.bmp'"'"
+watch -n 10 'thermalCam/thermalCam ; base64 thermalCam/testIR.bmp > thermalCam/testIR.b64 ; mosquitto_pub -h 192.168.0.22 -p 1883 -t '"'"'fusion/ir'"'"' -f '"'"'thermalCam/testIR.b64'"'"
