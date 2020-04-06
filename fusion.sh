389ea201676473c0d191d58d694ad76152db285f
Benjamin <benjamin.longa@gmail.com>
2020-04-06 14:30:35 +0200

    Probleme de path d'images

--- fusion.sh
+++ fusion.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch -n 10 'thermalCam/thermalCam ; base64 thermalCam/testIR.bmp > thermalCam/testIR.b64 ; mosquitto_pub -h 192.168.0.22 -p 1883 -t '"'"'fusion/ir'"'"' -f '"'"'thermalCam/testIR.b64'"'"
+watch -n 10 'thermalCam/thermalCam ; base64 testIR.bmp > testIR.b64 ; mosquitto_pub -h 192.168.0.22 -p 1883 -t '"'"'fusion/ir'"'"' -f '"'"'testIR.b64'"'"
