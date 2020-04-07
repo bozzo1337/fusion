a5071d65328181ab0124731aa5101be4758ff651
Benjamin <benjamin.longa@gmail.com>
2020-04-07 14:40:48 +0200

    syntax script

--- fusion.sh
+++ fusion.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-watch -n 10 'thermalCam/thermalCam ; base64 testIR.bmp > testIR.b64 ;
-mosquitto_pub -h 192.168.0.22 -p 1883 -t '"'"'fusion/ir'"'"' -f '"'"'testIR.b64'"'"' ; 
-curl -i -XPOST '"'"'http://192.168.0.22:8086/write?db=fusion'"'"' --data-binary @persondata.txt'"'"
+watch -n 10 'thermalCam/thermalCam ; base64 testIR.bmp > testIR.b64 ; mosquitto_pub -h 192.168.0.22 -p 1883 -t '"'"'fusion/ir'"'"' -f '"'"'testIR.b64'"'"' ; curl -i -XPOST '"'"'http://192.168.0.22:8086/write?db=fusion'"'"' --data-binary @persondata.txt'"'"
