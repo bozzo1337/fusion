4498d7742c5550a6dd35633433865e1c025f8383
Benjamin <benjamin.longa@gmail.com>
2020-04-07 15:11:30 +0200

    il faut recompiler c'est mieux

--- fusion.sh
+++ fusion.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+cd thermalCam
+make
+cd ..
 watch -n 10 'thermalCam/thermalCam ; base64 testIR.bmp > testIR.b64 ; mosquitto_pub -h 192.168.0.22 -p 1883 -t '"'"'fusion/ir'"'"' -f '"'"'testIR.b64'"'"' ; curl -i -XPOST '"'"'http://192.168.0.22:8086/write?db=fusion'"'"' --data-binary @persondata.txt'
